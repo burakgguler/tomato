cb15ffd1d0a545af520d967a17d15db59d1d2be7
Burak Güler <burak.guler02@bilgiedu.net>
2023-02-03 13:11:16 +0300

    caching restaurant list view 7.43

--- restaurant/api/urls.py
+++ restaurant/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from restaurant.api.views import (RestaurantListAPIView,
                                   RestaurantDetailAPIView,
@@ -8,7 +9,7 @@ from restaurant.api.views import (RestaurantListAPIView,
 app_name = 'restaurant'
 
 urlpatterns = [
-    path('list/', RestaurantListAPIView.as_view(), name='list'),
+    path('list/', cache_page(60 * 1)(RestaurantListAPIView.as_view()), name='list'),
     path('detail/<slug>/', RestaurantDetailAPIView.as_view(), name='detail'),
     path('update/<slug>/', RestaurantUpdateAPIView.as_view(), name='update'),
     path('create/', RestaurantCreateAPIView.as_view(), name='create')
