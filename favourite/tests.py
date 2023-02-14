7dc63baf5efd50f941516789c676b3e749493c0f
Burak Güler <burak.guler02@bilgiedu.net>
2023-02-15 01:34:56 +0300

    favourite list and create test case 8.48

--- favourite/tests.py
+++ favourite/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from favourite.models import Favourite
+from restaurant.models import Restaurant
+
+login_url = reverse("token_obtain_pair")
+
+
+class FavouriteCreateListTestCase(APITestCase):
+    favourite_url = reverse("favourite:list-create")
+
+    def setUp(self):
+        self.data = {
+            "username": "burak_fav",
+            "password": "test1234"
+        }
+        self.restaurant = Restaurant.objects.create(name="Fav", location="a", cuisine="b", slug="fav-test")
+        self.user = User.objects.create_user(username=self.data["username"], password=self.data["password"])
+        self.test_authentication()
+
+    def test_authentication(self):
+        response = self.client.post(login_url, self.data)
+        self.assertEqual(200, response.status_code)
+        token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+    def test_add_favourite(self):
+        data = {
+            "content": "fav testing",
+            "user": self.user.id,
+            "restaurant": self.restaurant.id
+        }
+        response = self.client.post(self.favourite_url, data)
+        self.assertEqual(201, response.status_code)
+
+    def test_list_favourites(self):
+        self.test_add_favourite()
+        response = self.client.get(self.favourite_url)
+        result_count = len(response.json()["results"])
+        test_user_fav_count = Favourite.objects.filter(user=self.user).count()
+        self.assertTrue(result_count == test_user_fav_count)
