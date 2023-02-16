73a91c65aea1711b490f025936f55a4e6ca64844
Burak Güler <burak.guler02@bilgiedu.net>
2023-02-17 01:25:18 +0300

    favourite update delete test case 8.49

--- favourite/tests.py
+++ favourite/tests.py
@@ -18,9 +18,9 @@ class FavouriteCreateListTestCase(APITestCase):
         }
         self.restaurant = Restaurant.objects.create(name="Fav", location="a", cuisine="b", slug="fav-test")
         self.user = User.objects.create_user(username=self.data["username"], password=self.data["password"])
-        self.test_authentication()
+        self.authentication()
 
-    def test_authentication(self):
+    def authentication(self):
         response = self.client.post(login_url, self.data)
         self.assertEqual(200, response.status_code)
         token = response.data["access"]
@@ -41,3 +41,37 @@ class FavouriteCreateListTestCase(APITestCase):
         result_count = len(response.json()["results"])
         test_user_fav_count = Favourite.objects.filter(user=self.user).count()
         self.assertTrue(result_count == test_user_fav_count)
+
+
+class FavouriteUpdateDeleteTestCase(APITestCase):
+    def setUp(self):
+        self.data = {
+            "username": "burak_fav_list",
+            "password": "test1234"
+        }
+        self.restaurant = Restaurant.objects.create(name="Fav", location="a", cuisine="b", slug="fav-test")
+        self.user = User.objects.create_user(username=self.data["username"], password=self.data["password"])
+        self.favourite = Favourite.objects.create(restaurant=self.restaurant, user=self.user)
+        self.update_url = reverse("favourite:update-delete", kwargs={"pk": self.favourite.pk})
+        self.authentication()
+
+    def authentication(self):
+        response = self.client.post(login_url, self.data)
+        self.assertEqual(200, response.status_code)
+        token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+    def test_favourite_delete(self):
+        response = self.client.delete(self.update_url)
+        self.assertEqual(204, response.status_code)
+
+    def test_favourite_update(self):
+        data = {
+            "content": "updated content!",
+            "restaurant": self.restaurant.id,
+            "user": self.user.id
+        }
+        response = self.client.put(self.update_url, data)
+        favourite_obj = Favourite.objects.get(id=self.favourite.id)
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(favourite_obj.content == data["content"])
