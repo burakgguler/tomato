1c8c83944da546219231048a4e46235a0ef4d8c1
Burak Güler <burak.guler02@bilgiedu.net>
2023-02-25 16:28:20 +0300

    admin and restaurant model modifications

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from restaurant.models import Cuisine, Restaurant, RestaurantCuisine
+from restaurant.models import Cuisine, Restaurant, Tag
 
-admin.site.register(Restaurant)
-admin.site.register(Cuisine)
-admin.site.register(RestaurantCuisine)
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'opening_time', 'closing_time', 'average_price')
+    list_filter = ('tags', 'cuisines')
+    exclude = ('slug',)
+
+
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('caption',)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,23 +1,49 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
+from rest_framework.exceptions import ValidationError
 
 from utils.models import StarterModel
 
 
+class Tag(StarterModel):
+    caption = models.CharField(max_length=30, verbose_name=_("Caption"))
+
+    def __str__(self):
+        return self.caption
+
+
+class Cuisine(StarterModel):
+    name = models.CharField(max_length=128, verbose_name=_("Cuisine Name"))
+
+    def __str__(self):
+        return self.name
+
+
 class Restaurant(StarterModel):
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1, related_name="created_restaurants")
-    modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="modified_restaurants")
-    name = models.CharField(max_length=120)
-    location = models.CharField(max_length=255)
-    slug = models.SlugField(db_index=True, unique=True, null=False)
-    phone = models.IntegerField(null=True)
-    opening_time = models.IntegerField(null=True)
-    closing_time = models.IntegerField(null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1,
+                                   verbose_name=_("Created By"), related_name="created_restaurants")
+    modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                    verbose_name=_("Modified By"), related_name="modified_restaurants")
+    tags = models.ManyToManyField(Tag, related_name="restaurants", verbose_name=_("Tags"))
+    cuisines = models.ManyToManyField(Cuisine, related_name="restaurants", verbose_name=_("Cuisines"))
+    name = models.CharField(max_length=120, verbose_name=_("Restaurant Name"))
+    location = models.CharField(max_length=255, verbose_name=_("Location"))
+    slug = models.SlugField(db_index=True, unique=True, null=False, verbose_name=_("Slug"))
+    phone = models.IntegerField(null=True, verbose_name=_("Phone Number"))
+    opening_time = models.TimeField(null=True, verbose_name=_("Opening Time"))
+    closing_time = models.TimeField(null=True, verbose_name=_("Closing Time"))
+    rating = models.DecimalField(max_digits=3, decimal_places=2, verbose_name=_("Rating"),
+                                 validators=[MinValueValidator(1), MaxValueValidator(5)], null=True, blank=True)
+    average_price = models.IntegerField(verbose_name=_("Average Price"), null=True, blank=True)
 
     class Meta:
         ordering = ['-id']
+
+    def __str__(self):
+        return self.name
 
     def get_slug(self):
         slug = slugify(self.name.replace('ı', 'i'))
@@ -30,27 +56,12 @@ class Restaurant(StarterModel):
 
         return unique
 
-    def __str__(self):
-        return self.name
+    def validate_times(self):
+        if self.closing_time <= self.opening_time:
+            raise ValidationError(detail=_("Closing time cannot be earlier than the opening time."))
 
     def save(self, *args, **kwargs):
         self.slug = self.get_slug()
+        self.validate_times()
+
         return super(Restaurant, self).save(*args, **kwargs)
-
-
-class Cuisine(StarterModel):
-    name = models.CharField(max_length=128, verbose_name=_("Cuisine Name"))
-
-    def __str__(self):
-        return self.name
-
-
-class RestaurantCuisine(StarterModel):
-    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name_plural = _("Restaurant Cuisines")
-
-    def __str__(self):
-        return self.restaurant.name + "/" + self.cuisine.name
